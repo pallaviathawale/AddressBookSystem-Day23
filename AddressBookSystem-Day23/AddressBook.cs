b37349760c1bbe1f8902e266300ac51d6b6edba5
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-31 14:19:31 +0530

    Add: UC4-DeleteContact

--- AddressBookSystem-Day23/AddressBook.cs
+++ AddressBookSystem-Day23/AddressBook.cs
@@ -111,6 +111,24 @@ namespace AddressBookSystem_Day23
                 }
             }
         }
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter the First Name to Check : ");
+            string firstName = Console.ReadLine();
+            foreach (Contact data in addressBookList)
+            {
+                if (data.FirstName == firstName)
+                {
+                    addressBookList.Remove(data);
+                    Console.WriteLine("Contact delete Scccessfully");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("No Contact Exists with this First Name : " + firstName);
+                }
+            }
+        }
         public void DisplayContact()
         {
             Console.WriteLine("Contact Details");
